2e46c044e46ac1b0667400fb56ec970cbb3975ea
gpensky <gabriel.pensky@gmail.com>
2024-04-16 08:22:21 -0300

    Remove whisper version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     url="https://github.com/gpensky/file-operations",
     packages=find_packages(),
     install_requires=[
-        "whisper @ git+ssh://git@github.com/openai/whisper@20231117#egg=whisper",
+        "whisper @ git+ssh://git@github.com/openai/whisper.git#egg=whisper",
         "pandas==1.3.4",
         "textract==1.5.0",
         "xmltodict==0.13.0",
